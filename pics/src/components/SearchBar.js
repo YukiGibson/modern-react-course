a1fa03a3e239caa7469f565eacea2c659d55e251
Laureano Sanchez <laureano.gabriel.aguiar@ibm.com>
2022-01-10 16:03:41 -0600

    Pass state upwards

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-
     state = { term: '' };
+
+    onFormSubmit(event) {
+        // prevent enter behavior
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={(event) => this.onFormSubmit(event)}>
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text" value={this.state.term} onChange={(e) => this.setState({term: e.target.value})}/>
